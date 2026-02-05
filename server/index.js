640dc5087aca7e3c32f6998452330100fb457206
Steve Dolinsky <stevendolinsky@gmail.com>
2026-02-04 23:58:06 -0500

    ui bug fixes

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(distPath));
 
   // Serve index.html for all non-API routes (SPA support)
-  app.get('*', (req, res) => {
+  app.get('{*path}', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
